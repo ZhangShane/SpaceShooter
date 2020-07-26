2df9e02c848c5a125b56e6f2c8b859666ba2ebfd
ZhangShane <437102045@qq.com>
2020-07-26 23:04:25 +0800

    unity3D project SpaceShooter

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    private Text scoreText;
+    private Text restartText;
+    private Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start ()
+    {
+        scoreText = GameObject.Find("Canvas/Score Text").GetComponent<Text>();
+        restartText = GameObject.Find("Canvas/Restart Text").GetComponent<Text>();
+        gameOverText = GameObject.Find("Canvas/Game Over Text").GetComponent<Text>();
+
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore ();
+        StartCoroutine (SpawnWaves ());
+    }
+
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds (spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore ();
+    }
+
+    void UpdateScore ()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
